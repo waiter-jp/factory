4e2049189343e0e5f25914ac475a9d4ee033c26c
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-11-29 15:12:06 +0000

    # WAITER-17 ### Added - 発行規則にクライアント設定を追加

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 /**
  * factory index
  */
+import * as client from './factory/client';
 import ErrorCode from './factory/errorCode';
 import * as errors from './factory/errors';
 import * as passport from './factory/passport';
 import * as project from './factory/project';
 import * as rule from './factory/rule';
 
+export import client = client;
 export import errorCode = ErrorCode;
 export import errors = errors;
 export import passport = passport;
